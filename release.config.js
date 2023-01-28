56ae747de2b95f47f8c64d9a0ac57f9ed069ddef
Zenith Wogwugwu <uzenith360@gmail.com>
2023-01-28 13:29:07 +0100

    fix: build issue where full path of dist/library folder is pushed to npm

--- release.config.js
+++ release.config.js
@@ -15,12 +15,14 @@ module.exports = {
           changelogFile: 'CHANGELOG.md'
         }
       ],
-      '@semantic-release/npm',
+      ["@semantic-release/npm", {
+        "pkgRoot": "dist/ngx-jwt-auth",
+      }],
       '@semantic-release/github',
       [
         '@semantic-release/git',
         {
-          assets: ['CHANGELOG.md', 'dist/ngx-jwt-auth/**'],
+          assets: ['CHANGELOG.md', 'dist/**'],
           message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }
       ]
