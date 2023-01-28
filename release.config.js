807c179280abc0fc19aea2c3aa457b92b751c426
Zenith Wogwugwu <uzenith360@gmail.com>
2023-01-28 14:39:55 +0100

    fix(ngx-jwt-auth): build issue where full path of dist/library folder is pushed to npm

--- release.config.js
+++ release.config.js
@@ -1,29 +1,28 @@
-module.exports = {
-    pkgRoot: 'dist/@ngx-jwt-auth/angular',
-    branches: [
-      'main',
-      {
-        name: 'beta',
-        prerelease: true
-      }
-    ],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        '@semantic-release/changelog',
-        {
-          changelogFile: 'CHANGELOG.md'
-        }
-      ],
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'dist/**'],
-          message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ]
-    ]
-  }
+// module.exports = {
+//     branches: [
+//       'main',
+//       {
+//         name: 'beta',
+//         prerelease: true
+//       }
+//     ],
+//     plugins: [
+//       '@semantic-release/commit-analyzer',
+//       '@semantic-release/release-notes-generator',
+//       [
+//         '@semantic-release/changelog',
+//         {
+//           changelogFile: 'CHANGELOG.md'
+//         }
+//       ],
+//       '@semantic-release/npm',
+//       '@semantic-release/github',
+//       [
+//         '@semantic-release/git',
+//         {
+//           assets: ['CHANGELOG.md', 'dist/**'],
+//           message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+//         }
+//       ]
+//     ]
+//   }
